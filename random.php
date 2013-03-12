e80c8233ca65e0b1e62f3bda106705a2451681ee
fastpoke <fastpoke@fastpoke.org>
2013-03-13 04:21:27 +0700

    nyaa~ random engine fix

--- random.php
+++ random.php
@@ -1,6 +1,7 @@
 <?php
 
 	$folder = '/foo/bar/';
+	$index = $folder.'index.sqlite';
 	$extList = array();
 	$extList['gif'] = 'image/gif';
 	$extList['jpg'] = 'image/jpeg';
@@ -22,32 +23,13 @@ if (isset($_GET['img'])) {
 	}
 } else {
 
-	function rand_line($fileName, $maxLineLength = 4096) {
-    $handle = @fopen($fileName, "r");
-    if ($handle) {
-        $random_line = null;
-        $line = null;
-        $count = 0;
-        while (($line = fgets($handle, $maxLineLength)) !== false) {
-            $count++;
-            if(rand() % $count == 0) {
-              $random_line = $line;
-            }
-        }
-        if (!feof($handle)) {
-            echo "Error: unexpected fgets() fail";
-            fclose($handle);
-            return null;
-        } else {
-            fclose($handle);
-        }
-        return $random_line;
-    }
+    $imageIndex = file($index);
+    $imageRandom = array_rand($imageIndex);
+    $imageResult = $imageIndex[$imageRandom];
 }
 
-	$fileList = trim(preg_replace('/[\r\n]+/m','',rand_line("/foo/bar/index.ls"))); 
+	$fileList = trim(preg_replace('/[\r\n]+/m','',$imageResult)); 
 	$img = $folder.$fileList;
-}
 
 if ($img!=null) {
 	$imageInfo = pathinfo($img);
@@ -56,6 +38,7 @@ if ($img!=null) {
 	header ($contentType);
 	header ($name);
 	readfile ($img);
+
 } else {
 	if ( function_exists('imagecreate') ) {
 		header ('Content-type: image/png');
