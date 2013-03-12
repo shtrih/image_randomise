c20a62cda60991921c323cd193d6e54e2b292b13
fastpoke <fastpoke@fastpoke.org>
2013-03-13 03:51:29 +0700

    fix search pattern

--- image_index.sh
+++ image_index.sh
@@ -2,7 +2,7 @@
 tmpfile=`mktemp`
 dir=/foo/bar
 
-cd $dir && find . -type f | sed 's/^.\///g' | egrep "png|PNG|jpg|JPG|jpeg|JPEG|gif|GIF" | sort > $tmpfile
+cd $dir && find . -type f | sed 's/^.\///g' | egrep -i "png|jpg|jpeg|gif" | sort > $tmpfile
 mv $tmpfile $dir/index.ls && chmod 775 $dir/index.ls
 
 exit 0
