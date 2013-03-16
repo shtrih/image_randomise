074b261cbdee64fd4e4eb9bb32e978ec92b3afcf
fastpoke <fastpoke@fastpoke.org>
2013-03-16 13:55:14 +0700

    say hello~

--- image_index.sh
+++ image_index.sh
@@ -2,7 +2,7 @@
 tmpfile=`mktemp`
 dir=/foo/bar
 
-cd $dir && find . -type f | sed 's/^.\///g' | egrep -i "png|jpg|jpeg|gif" | sort > $tmpfile
+cd $dir && find . -type f | egrep -i "png|jpg|jpeg|gif" | sed 's/^.\///g' | sort > $tmpfile
 mv $tmpfile $dir/index.ls && chmod 775 $dir/index.ls
 
 exit 0
