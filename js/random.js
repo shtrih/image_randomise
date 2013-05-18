28a6467f10f18e8607b2b2bcf9a80908b195efb4
shtrih <shtrih@hitagi.ru>
2013-05-19 01:47:40 +0900

    Feature: добавил интеграцию с history api

--- js/random.js
+++ js/random.js
@@ -1,24 +1,36 @@
 $(function () {
+	var overlay = $('.overlay');
+		imageloader = new Image();
+
 	$('.reload,.img a').on('click', function () {
-		var overlay = $('.overlay').fadeIn('fast');
-			imageloader = new Image();
+		overlay.fadeIn('fast');
 
 		$.getJSON('', {ajax:true}, function (data) {
-			imageloader.onload = function () {
-				$('.img img')
-					.attr('src', data.url)
-					.attr('alt', data.name)
-					.attr('title', data.name);
-				overlay.fadeOut('fast');
-			};
-			imageloader.src = data.url;
-			$('.gplus').attr('href', 'https://plus.google.com/share?url=' + encodeURI(data.url));
-			$('#copy').val(data.url);
-			$('.info span').html(data.resolution[0] + ' &times; ' + data.resolution[1] + ' px &nbsp;&nbsp;@&nbsp;&nbsp; ' + data.size + ' Kb');
+			applyData(data);
+			history.pushState(data, null, document.location.pathname + '?img=' + data.name);
 		});
 
 		return false;
 	});
 
+	function applyData(data, push) {
+		imageloader.onload = function () {
+			$('.img img')
+				.attr('src', data.url)
+				.attr('alt', data.name)
+				.attr('title', data.name);
+			overlay.fadeOut('fast');
+		};
+		imageloader.src = data.url;
+		$('.gplus').attr('href', 'https://plus.google.com/share?url=' + encodeURI(data.url));
+		$('#copy').val(data.url);
+		$('.info span').html(data.resolution[0] + ' &times; ' + data.resolution[1] + ' px &nbsp;&nbsp;@&nbsp;&nbsp; ' + data.size + ' Kb');
+	}
 
+	window.addEventListener("popstate", function(e) {
+		if (history.state){
+			console.log(location.pathname, e.state);
+			applyData(e.state);
+		}
+	});
 });
